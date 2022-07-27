2d972a7e78029d78975427a8c631dde4a901ef1e
LuigiZannella <zannellaluigi@outlook.com>
2022-07-27 14:04:48 +0200

    Esercizio completato

---
+++ src/main/java/jana60/repository/PizzaRepository.java
@@ -0,0 +1,12 @@
+package jana60.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import jana60.model.Pizza;
+
+@Repository
+public interface PizzaRepository extends CrudRepository<Pizza, Integer>{
+
+	public Integer countByNome(String nome);
+}
